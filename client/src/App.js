f3848b0385855d8e53b033d3278e442e49d10a6e
Keerthana <keersathya3111@gmail.com>
2024-03-15 00:01:50 +0530

    added frontend

---
+++ client/src/App.js
@@ -0,0 +1,53 @@
+import "./App.css";
+import Home from "./Home";
+import React from 'react';
+import TrazLogo from "./images/traz_logo.png"
+import DigitalIndia from "./images/digital_india.png"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+
+const Navbar = () => {
+  return (
+     <div className='navbar'> 
+            <div className='logo'>
+              <img src={TrazLogo} alt="logo"/>
+              <h3 style={{"fontWeight": "bold"}}>X</h3>
+              <img src={DigitalIndia} alt="logo" />
+            </div>
+            <div className='nav-links'>
+                <Link to="/">
+                  Home
+                </Link>
+                <Link to="/roles">
+                  Roles
+                </Link>
+                <Link to="/addmed">
+                  Add Product
+                </Link>
+                <Link to="/supply">
+                  Supply
+                </Link>
+                <Link to="/track">
+                  Track
+                </Link>
+            </div>
+        </div>
+  )
+}
+
+export default App;
