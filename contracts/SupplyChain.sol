b7b1c7e4c806763c4a9d85f1c4ebfd2a25b23690
Tharun Balaji R <tharunbalaji31@gmail.com>
2024-03-15 15:54:22 +0530

    Modified smart contracts and css

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -1,32 +1,26 @@
-pragma solidity >=0.4.22 < 0.9.0;
+pragma solidity >=0.4.22 <0.9.0;
 
-// Created contract model
 contract SupplyChain {
-
-    // stores owner address
     address public Owner;
 
     constructor() public {
         Owner = msg.sender;
-
     }
-
     modifier onlyByOwner() {
         require(msg.sender == Owner);
         _;
     }
 
-
     enum STAGE {
-        Init, 
-        RawMaterialStage,
+        Init,
+        RawMaterialSupply,
         Manufacture,
         Distribution,
         Retail,
-        Sold
+        sold
     }
 
-    uint256 public medicineCtr = 0;
+    uint256 public productCtr = 0;
     uint256 public rmsCtr = 0;
     uint256 public manCtr = 0;
     uint256 public disCtr = 0;
@@ -34,16 +28,15 @@ contract SupplyChain {
 
     //To store information about the product
     struct product {
-        uint256 id; 
-        string name; 
+        uint256 id;
+        string name;
         string description; 
-        uint256 RMSid;
+        uint256 RMSid; 
         uint256 MANid;
         uint256 DISid;
         uint256 RETid;
         STAGE stage;
     }
-
     mapping(uint256 => product) public ProductStock;
 
     //To show status to client applications
@@ -52,7 +45,7 @@ contract SupplyChain {
         view
         returns (string memory)
     {
-        require(medicineCtr > 0);
+        require(productCtr > 0);
         if (ProductStock[_productID].stage == STAGE.Init)
             return "Product Ordered";
         else if (ProductStock[_productID].stage == STAGE.RawMaterialSupply)
@@ -67,7 +60,6 @@ contract SupplyChain {
             return "Product Sold";
     }
 
-    //Raw material supplier
     struct rawMaterialSupplier {
         address addr;
         uint256 id;
@@ -77,18 +69,15 @@ contract SupplyChain {
 
     mapping(uint256 => rawMaterialSupplier) public RMS;
 
-    //Manufacturer
+    //To store information about manufacturer
     struct manufacturer {
         address addr;
-        uint256 id;
+        uint256 id; 
         string name; 
         string place;
     }
-
     mapping(uint256 => manufacturer) public MAN;
 
-
-    // Distributor
     struct distributor {
         address addr;
         uint256 id; 
@@ -97,15 +86,182 @@ contract SupplyChain {
     }
 
     mapping(uint256 => distributor) public DIS;
-
-    // Retailer
     struct retailer {
         address addr;
         uint256 id;
-        string name; 
+        string name;
         string place;
     }
 
     mapping(uint256 => retailer) public RET;
 
+    function addRMS(
+        address _address,
+        string memory _name,
+        string memory _place
+    ) public onlyByOwner() {
+        rmsCtr++;
+        RMS[rmsCtr] = rawMaterialSupplier(_address, rmsCtr, _name, _place);
+    }
+
+    function addManufacturer(
+        address _address,
+        string memory _name,
+        string memory _place
+    ) public onlyByOwner() {
+        manCtr++;
+        MAN[manCtr] = manufacturer(_address, manCtr, _name, _place);
+    }
+
+    function addDistributor(
+        address _address,
+        string memory _name,
+        string memory _place
+    ) public onlyByOwner() {
+        disCtr++;
+        DIS[disCtr] = distributor(_address, disCtr, _name, _place);
+    }
+
+    function addRetailer(
+        address _address,
+        string memory _name,
+        string memory _place
+    ) public onlyByOwner() {
+        retCtr++;
+        RET[retCtr] = retailer(_address, retCtr, _name, _place);
+    }
+
+    function RMSsupply(uint256 _productID) public {
+        require(_productID > 0 && _productID <= productCtr);
+        uint256 _id = findRMS(msg.sender);
+        require(_id > 0);
+        require(ProductStock[_productID].stage == STAGE.Init);
+        ProductStock[_productID].RMSid = _id;
+        ProductStock[_productID].stage = STAGE.RawMaterialSupply;
+    }
+
+    function findRMS(address _address) private view returns (uint256) {
+        require(rmsCtr > 0);
+        for (uint256 i = 1; i <= rmsCtr; i++) {
+            if (RMS[i].addr == _address) return RMS[i].id;
+        }
+        return 0;
+    }
+
+    function Manufacturing(uint256 _productID) public {
+        require(_productID > 0 && _productID <= productCtr);
+        uint256 _id = findMAN(msg.sender);
+        require(_id > 0);
+        require(ProductStock[_productID].stage == STAGE.RawMaterialSupply);
+        ProductStock[_productID].MANid = _id;
+        ProductStock[_productID].stage = STAGE.Manufacture;
+    }
+
+    function findMAN(address _address) private view returns (uint256) {
+        require(manCtr > 0);
+        for (uint256 i = 1; i <= manCtr; i++) {
+            if (MAN[i].addr == _address) return MAN[i].id;
+        }
+        return 0;
+    }
+
+    function Distribute(uint256 _productID) public {
+        require(_productID > 0 && _productID <= productCtr);
+        uint256 _id = findDIS(msg.sender);
+        require(_id > 0);
+        require(ProductStock[_productID].stage == STAGE.Manufacture);
+        ProductStock[_productID].DISid = _id;
+        ProductStock[_productID].stage = STAGE.Distribution;
+    }
+
+    function findDIS(address _address) private view returns (uint256) {
+        require(disCtr > 0);
+        for (uint256 i = 1; i <= disCtr; i++) {
+            if (DIS[i].addr == _address) return DIS[i].id;
+        }
+        return 0;
+    }
+
+    function Retail(uint256 _productID) public {
+        require(_productID > 0 && _productID <= productCtr);
+        uint256 _id = findRET(msg.sender);
+        require(_id > 0);
+        require(ProductStock[_productID].stage == STAGE.Distribution);
+        ProductStock[_productID].RETid = _id;
+        ProductStock[_productID].stage = STAGE.Retail;
+    }
+
+    function findRET(address _address) private view returns (uint256) {
+        require(retCtr > 0);
+        for (uint256 i = 1; i <= retCtr; i++) {
+            if (RET[i].addr == _address) return RET[i].id;
+        }
+        return 0;
+    }
+    function sold(uint256 _productID) public {
+        require(_productID > 0 && _productID <= productCtr);
+        uint256 _id = findRET(msg.sender);
+        require(_id > 0);
+        require(_id == ProductStock[_productID].RETid); //Only correct retailer can mark product as sold
+        require(ProductStock[_productID].stage == STAGE.Retail);
+        ProductStock[_productID].stage = STAGE.sold;
+    }
+
+    function addProduct(string memory _name, string memory _description)
+        public
+        onlyByOwner()
+    {
+        require((rmsCtr > 0) && (manCtr > 0) && (disCtr > 0) && (retCtr > 0));
+        productCtr++;
+        ProductStock[productCtr] = product(
+            productCtr,
+            _name,
+            _description,
+            0,
+            0,
+            0,
+            0,
+            STAGE.Init
+        );
+    }
+
+    function getProductData(uint256 _productCounter) public view returns (
+        uint256 id,
+        string memory name,
+        string memory description,
+        uint256 RMSid,
+        uint256 MANid,
+        uint256 DISid,
+        uint256 RETid,
+        string memory stage
+    ) {
+        require(_productCounter > 0 && _productCounter <= productCtr, "Invalid product counter");
+        
+        product memory med = ProductStock[_productCounter];
+        
+        id = med.id;
+        name = med.name;
+        description = med.description;
+        RMSid = med.RMSid;
+        MANid = med.MANid;
+        DISid = med.DISid;
+        RETid = med.RETid;
+
+        if (med.stage == STAGE.Init)
+            stage = "Product Ordered";
+        else if (med.stage == STAGE.RawMaterialSupply)
+            stage = "Raw Material Supply Stage";
+        else if (med.stage == STAGE.Manufacture)
+            stage = "Manufacturing Stage";
+        else if (med.stage == STAGE.Distribution)
+            stage = "Distribution Stage";
+        else if (med.stage == STAGE.Retail)
+            stage = "Retail Stage";
+        else if (med.stage == STAGE.sold)
+            stage = "Product Sold";
+    }
+
+    function getLatestProductCounter() public view returns (uint256) {
+        return productCtr;
+    }
 }
